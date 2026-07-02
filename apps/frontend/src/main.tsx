6b28a48841584a35f19b40b23183cde84ac45e65
Fabribrollo <fabrizio.brollo@est.fi.uncoma.edu.ar>
2026-07-02 13:56:24 -0300

    Arregle errores de electron

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Toaster } from "@/components/ui/sonner";
 import App from "./App.tsx";
 import "./index.css";
@@ -19,10 +19,10 @@ const queryClient = new QueryClient({
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
         <Toaster />
-      </BrowserRouter>
+      </HashRouter>
     </QueryClientProvider>
   </StrictMode>,
 );
